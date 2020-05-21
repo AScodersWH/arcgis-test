322244126a8bcd167c131d1e26524eb9db9d5c97
yukivkaras <497321931@qq.com>
2020-05-22 04:41:31 +0800

    great

--- src/components/arcPage.js
+++ src/components/arcPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Layout, Menu, Breadcrumb } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { UserOutlined, LaptopOutlined, NotificationOutlined, LineChartOutlined,AreaChartOutlined,EnvironmentFilled } from '@ant-design/icons';
 import './arcPage.css'
 import 'antd/dist/antd.css'; 
 const { SubMenu } = Menu;
@@ -12,9 +12,10 @@ render(){
     <Header className="header">
       <div className="logo" />
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
+      <Menu.Item key="0" style={{position:"relative",right:"44%"}}>海底滑坡监测预警系统</Menu.Item>
+        <Menu.Item key="1">数据</Menu.Item>
+        <Menu.Item key="2">图片</Menu.Item>
+
       </Menu>
     </Header>
     <Layout>
@@ -22,44 +23,42 @@ render(){
         <Menu
           mode="inline"
           defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
+          defaultOpenKeys={['sub0']}
           style={{ height: '100%', borderRight: 0 }}
         >
-          <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-            <Menu.Item key="1">option1</Menu.Item>
-            <Menu.Item key="2">option2</Menu.Item>
-            <Menu.Item key="3">option3</Menu.Item>
-            <Menu.Item key="4">option4</Menu.Item>
+            <SubMenu key="sub0" icon={<EnvironmentFilled />} title="研究区概况"/>
+ 
+          <SubMenu key="sub1" icon={<AreaChartOutlined />}  title="地质环境要素">
+            <Menu.Item key="1">水深测量成果图</Menu.Item>
+            <Menu.Item key="2">地貌图</Menu.Item>
+            <Menu.Item key="3">钻孔图</Menu.Item>
+            <Menu.Item key="4">经纬度</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-            <Menu.Item key="5">option5</Menu.Item>
-            <Menu.Item key="6">option6</Menu.Item>
-            <Menu.Item key="7">option7</Menu.Item>
-            <Menu.Item key="8">option8</Menu.Item>
+          <SubMenu key="sub2" icon={<LaptopOutlined />} title="数据展示">
+            <Menu.Item key="5">孔压</Menu.Item>
+            <Menu.Item key="6">流速</Menu.Item>
+            <Menu.Item key="7">海床滑动变形</Menu.Item>
+            <Menu.Item key="8">波浪</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-            <Menu.Item key="9">option9</Menu.Item>
-            <Menu.Item key="10">option10</Menu.Item>
-            <Menu.Item key="11">option11</Menu.Item>
-            <Menu.Item key="12">option12</Menu.Item>
+          <SubMenu key="sub3" icon={<LineChartOutlined />} title="数据分析">
+            
+          </SubMenu>
+          <SubMenu key="sub3" icon={<NotificationOutlined />} title="预警信息发布">
+          
           </SubMenu>
         </Menu>
       </Sider>
       <Layout style={{ padding: '0 24px 24px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
+        
         <Content
           className="site-layout-background"
           style={{
             padding: 24,
             margin: 0,
-            minHeight: 280,
+            minHeight: 680,
+            
           }}
         >
-          Content
         </Content>
       </Layout>
     </Layout>
